f970c667b5b4be765a82ae5f2038b44d904cb360
Luckahodik <lucka.hodnik@gmail.com>
2019-06-22 00:08:50 +0200

    dodan sequelize ORM

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const db = new Sequelize('postgres', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,'/www/index.html'))
 })
